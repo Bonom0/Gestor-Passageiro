a1585f69c658c5b31c0841df8c53e9e312fe7918
caio <caioazbonomo@gmail.com>
2025-03-12 19:46:33 -0300

    Criado createPassageiroDto para trafegar no payload das requisições

--- src/passageiro/dto/create-passageiro.dto.ts
+++ src/passageiro/dto/create-passageiro.dto.ts
@@ -1,1 +1,38 @@
-export class CreatePassageiroDto {}
+import { 
+    IsBoolean, 
+    IsString, 
+    IsDate, 
+    IsInt, 
+    IsNumber 
+} from 'class-validator'
+
+//objeto que vai ser trafegado no payload das requisições
+
+export class CreatePassageiroDto {
+    @IsString()
+    nome: string;
+
+    @IsString()
+    cpf: string;
+
+    @IsString()
+    senha: string;
+    
+    @IsString()
+    cep: string;
+
+    @IsString()
+    rua: string;
+
+    @IsString()
+    contato: string;
+
+    @IsDate()
+    horario_embarque: Date;
+
+    @IsString()
+    id_motorista: string;
+
+    @IsBoolean()
+    ativo: boolean;
+}
