b2551570207904c445068a1fd26ae1fd1dcb87e5
caio <caioazbonomo@gmail.com>
2025-03-18 20:38:46 -0300

    Adicionado função mapToEntity para fazer parse do banco e corrigido função findAll

--- src/passageiro/passageiro.service.ts
+++ src/passageiro/passageiro.service.ts
@@ -6,17 +6,34 @@ import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
 export class PassageiroService {
+  constructor(private prisma: PrismaService) {}
 
-  constructor(private prisma: PrismaService){
-
+  private mapToEntity(passageiro: any): Passageiro {
+    //pega o obj do banco e faz um parse para o modelo do nest
+    return {
+      id: passageiro.id,
+      nome: passageiro.nome,
+      cpf: passageiro.cpf,
+      senha: passageiro.senha,
+      cep: passageiro.cep,
+      rua: passageiro.rua,
+      contato: passageiro.contato,
+      horario_embarque: passageiro.horario_embarque,
+      id_motorista: passageiro.id_motorista,
+      ativo: passageiro.ativo,
+      dta_insert: passageiro.dta_insert,
+      tipo: passageiro.tipo,
+    };
   }
 
   create(createPassageiroDto: CreatePassageiroDto) {
     return 'This action adds a new passageiro';
   }
 
-  findAll() {
-    return `This action returns all passageiro`;
+  async findAll(): Promise<Passageiro[]> {
+    //faz a busca de todos os passageiros no banco de dados
+    const passageiro = await this.prisma.passageiro.findMany(); //faz a busca de todos os objs no banco
+    return passageiro.map((passageiro) => this.mapToEntity(passageiro)); //map faz o parse do obj
   }
 
   findOne(id: number) {
