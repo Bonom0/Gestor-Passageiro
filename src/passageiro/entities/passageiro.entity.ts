6c101529979d1137486346a695a6af908aeb7c06
caio <caioazbonomo@gmail.com>
2025-03-18 19:35:30 -0300

    Adicionado os campos dta_insert e tipo

--- src/passageiro/entities/passageiro.entity.ts
+++ src/passageiro/entities/passageiro.entity.ts
@@ -1,12 +1,14 @@
 export class Passageiro {
-    id: string;
-    nome: string;
-    cpf: string;
-    senha: string;
-    cep: string;
-    rua: string;
-    contato: string;
-    horario_embarque: Date;
-    id_motorista: string;
-    ativo: boolean;
+  id: string;
+  nome: string;
+  cpf: string;
+  senha: string;
+  cep: string;
+  rua: string;
+  contato: string;
+  horario_embarque: Date;
+  id_motorista: string;
+  ativo: boolean;
+  dta_insert: Date;
+  tipo: string;
 }
