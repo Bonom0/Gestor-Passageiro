07356b5827c81a175a1e5f74cb58b8d6f9b165fb
caio <caioazbonomo@gmail.com>
2025-04-16 19:38:00 -0300

    Adicionado campos que estavam faltando

--- src/passageiro/dto/create-passageiro.dto.ts
+++ src/passageiro/dto/create-passageiro.dto.ts
@@ -1,38 +1,41 @@
-import { 
-    IsBoolean, 
-    IsString, 
-    IsDate, 
-    IsInt, 
-    IsNumber 
-} from 'class-validator'
+import { IsBoolean, IsString, IsDate } from 'class-validator';
 
 //objeto que vai ser trafegado no payload das requisições
 
 export class CreatePassageiroDto {
-    @IsString()
-    nome: string;
+  @IsString()
+  nome: string;
 
-    @IsString()
-    cpf: string;
+  @IsString()
+  cpf: string;
 
-    @IsString()
-    senha: string;
-    
-    @IsString()
-    cep: string;
+  @IsString()
+  senha: string;
 
-    @IsString()
-    rua: string;
+  @IsString()
+  cep: string;
 
-    @IsString()
-    contato: string;
+  @IsString()
+  rua: string;
 
-    @IsDate()
-    horario_embarque: Date;
+  @IsString()
+  contato: string;
 
-    @IsString()
-    id_motorista: string;
+  @IsDate()
+  horario_embarque: Date;
 
-    @IsBoolean()
-    ativo: boolean;
+  @IsString()
+  id_motorista: string;
+
+  @IsBoolean()
+  ativo: boolean;
+
+  @IsDate()
+  dta_insert: Date;
+
+  @IsString()
+  tipo: string;
+
+  @IsString()
+  email: string;
 }
