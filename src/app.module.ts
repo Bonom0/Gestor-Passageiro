8d660a234a27cfd0b8bd8fbd9da2123c3ba11624
caio <caioazbonomo@gmail.com>
2025-06-04 20:04:37 -0300

    Importado EmpresaModule e TipoUsuarioModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PassageiroModule } from './passageiro/passageiro.module';
+import { EmpresaModule } from './empresa/empresa.module';
+import { TipoUsuarioModule } from './tipousuario/tipousuario.module';
 
 @Module({
-  imports: [PassageiroModule],
+  imports: [PassageiroModule, EmpresaModule, TipoUsuarioModule],
   controllers: [AppController],
   providers: [AppService],
 })
