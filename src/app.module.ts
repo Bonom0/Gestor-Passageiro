f8a2246b9ee8a6033edaa11e1fcbaeb24daae78f
caio <caioazbonomo@gmail.com>
2025-06-17 18:58:23 -0300

    Importado EmpresaModule e OperadorModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,16 @@ import { AppService } from './app.service';
 import { PassageiroModule } from './passageiro/passageiro.module';
 import { EmpresaModule } from './empresa/empresa.module';
 import { TipoUsuarioModule } from './tipousuario/tipousuario.module';
+import { OperadorModule } from './operador/operador.module';
 
 @Module({
-  imports: [PassageiroModule, EmpresaModule, TipoUsuarioModule],
+  imports: [
+    PassageiroModule,
+    EmpresaModule,
+    TipoUsuarioModule,
+    OperadorModule,
+    EmpresaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
