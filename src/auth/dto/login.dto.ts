9fb5ea7289a46b975589893f2a6d305df9367732
Bonom0 <caioazbonomo@gmail.com>
2025-06-23 12:01:58 -0300

    Adicionado validação para os campos de login

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,5 +1,20 @@
+import { IsEmail, IsEnum, IsNotEmpty, IsString } from 'class-validator';
+
+export enum UserType {
+  Operador = 'Operador',
+  Motorista = 'Motorista',
+  Passageiro = 'Passageiro',
+}
+
 export class LoginDto {
-  userType: 'Operador' | 'Motorista' | 'Passageiro';
+  @IsEnum(UserType, { message: 'Tipo de usuário inválido' })
+  userType: UserType;
+
+  @IsNotEmpty({ message: 'Email é obrigatório' })
+  @IsEmail({}, { message: 'Email inválido' })
   email: string;
+
+  @IsNotEmpty({ message: 'Senha é obrigatória' })
+  @IsString()
   password: string;
 }
